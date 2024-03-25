b925f70393b441db7449a6bae2fe906763861caf
Alejandro Marco <alexmarco18795@gmail.com>
2024-03-25 23:28:12 +0100

    ✨ Add search by multiple ingredients
    - Add new fixture
    - make sure there are no duplicates on search
    - make search case-insensitive

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,7 +5,10 @@ class Recipe < ApplicationRecord
   validates :cook_time, :prep_time, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
   # We can add more validations here...
 
-  def self.search_by_ingredient(ingredient_name)
-    joins(:ingredients).where(ingredients: { name: ingredient_name })
+  def self.search_by_ingredient(ingredient_names)
+    ingredient_names = [ingredient_names].flatten.map(&:downcase)
+    joins(:ingredients)
+      .where('LOWER(ingredients.name) IN (?)', ingredient_names)
+      .distinct # This is to avoid repeating the same recipe if it has multiple ingredients in the search query
   end
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -35,4 +35,51 @@ class RecipeTest < ActiveSupport::TestCase
     assert_includes found_recipes, recipe_with_flour, "Should find recipes containing flour"
     assert_includes found_recipes, another_recipe_with_flour, "Should find recipes containing flour"
   end
+
+  test "should find recipes by multiple ingredient names" do
+    ingredient_one = ingredients(:cocoa_powder)
+    ingredient_two = ingredients(:magic)
+    recipe_with_cocoa = recipes(:chocolate_cake) # Fixture with cocoa
+    recipe_with_magic = recipes(:magic_pizza) # Fixture with magic
+
+    found_recipes = Recipe.search_by_ingredient([ingredient_one.name, ingredient_two.name])
+
+    assert_includes found_recipes, recipe_with_cocoa, "Should find recipes containing flour"
+    assert_includes found_recipes, recipe_with_magic, "Should find recipes containing milk"
+  end
+
+  test "should find recipes that have multiple ingredients searched without repeating them" do
+    ingredient_one = ingredients(:cocoa_powder)
+    ingredient_two = ingredients(:milk)
+    recipe_with_cocoa_and_milk = recipes(:chocolate_cake) # Fixture with cocoa
+
+    found_recipes = Recipe.search_by_ingredient([ingredient_one.name, ingredient_two.name])
+
+    # It should only find the recipe that contains both cocoa and milk, the chocolate cake, and just once
+    assert_equal 1, found_recipes.count
+    assert_includes found_recipes, recipe_with_cocoa_and_milk, "Should find recipes containing flour"
+  end
+
+  test "should find recipes only one even if you repeat the ingredient" do
+    ingredient_one = ingredients(:cocoa_powder)
+    recipe_with_cocoa_and_milk = recipes(:chocolate_cake) # Fixture with cocoa
+
+    found_recipes = Recipe.search_by_ingredient([ingredient_one.name, ingredient_one.name])
+
+    # It should only find the recipe that contains both cocoa and milk, the chocolate cake, and just once
+    assert_equal 1, found_recipes.count
+    assert_includes found_recipes, recipe_with_cocoa_and_milk, "Should find recipes containing flour"
+  end
+
+  test "should find recipes by ingredient name case-insensitive" do
+    ingredient = ingredients(:flour)
+    recipe_with_flour = recipes(:golden_cornbread)
+    another_recipe_with_flour = recipes(:chocolate_cake)
+
+    found_recipes = Recipe.search_by_ingredient(ingredient.name.upcase)
+
+    # It should find both recipes that contain flour
+    assert_includes found_recipes, recipe_with_flour, "Should find recipes containing flour"
+    assert_includes found_recipes, another_recipe_with_flour, "Should find recipes containing flour"
+  end
 end
